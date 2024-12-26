5543c8cc948cbdd385a7e56cb23bfdea93ddde70
Riii3393 <aleph.0.leaf@gmail.com>
2024-12-26 12:23:45 +0900

    Update energizedpower.js

--- kubejs/server_scripts/recipe/energizedpower.js
+++ kubejs/server_scripts/recipe/energizedpower.js
@@ -2,7 +2,7 @@ ServerEvents.recipes(event => {
     event.shapeless(
         Item.of('kubejs:diamond_plate', 1), // arg 1: output
         [
-          '#forge:hammers',
+          '#forge:tools/hammers',
           '#forge:gems/diamond'
         ]
       )
